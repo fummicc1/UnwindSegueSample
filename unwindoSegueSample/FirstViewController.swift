b79bae6d5b47580c29ab4e5fde27337ad3fa1971
fummicc1 <fumiya989@gmail.com>
2019-02-13 16:38:19 +0900

    commit

--- unwindoSegueSample/FirstViewController.swift
+++ unwindoSegueSample/FirstViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FirstViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func unwind(sender: UIStoryboardSegue) {
+        
+    }
+    
 }
 
---
+++ unwindoSegueSample/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  unwindoSegueSample
+//
+//  Created by Fumiya Tanaka on 2019/02/13.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+}
